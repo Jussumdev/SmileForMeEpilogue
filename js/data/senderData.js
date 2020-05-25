1d27a88eebdcd3a6e858cbcd3b38335d752c434a
yugsly <bronxted@gmail.com>
2020-05-24 22:17:06 -0700

    Icons

--- js/data/senderData.js
+++ js/data/senderData.js
@@ -18,35 +18,227 @@ name:               'Clancy Cat Soup'
 ,profPicSource:     'test.png'
 }
 
-var CopyThisAndChangeTheName = {
-name:               ''
-,email:             ''
-,profPicName:       ''
-,profPicAuthor:     ''
-,profPicLink:       ''
-,profPicAuthorLink: ''
-,profPicSource:     ''
+var Jimothan = {
+name:               'Jimothan Botch'
+,email:             'grillmaster3@lukewarmmail.sizzle'
+,profPicName:       'Jimothan, but very small'
+,profPicAuthor:     'Nightverse'
+,profPicLink:       'https://nightverse.tumblr.com/post/188078754315'
+,profPicAuthorLink: 'https://nightverse.tumblr.com'
+,profPicSource:     'jimothanicon.png'
 }
 
-var CopyThisAndChangeTheName = {
-name:               ''
-,email:             ''
-,profPicName:       ''
-,profPicAuthor:     ''
-,profPicLink:       ''
-,profPicAuthorLink: ''
-,profPicSource:     ''
+var Millie = {
+name:               'Millie Coulro'
+,email:             'killerclownzz@slappin.bspace'
+,profPicName:       'Millie with a mischievous face'
+,profPicAuthor:     'Sol'
+,profPicLink:       'https://eggceereal.tumblr.com'
+,profPicAuthorLink: 'https://eggceereal.tumblr.com/post/186759623006/may-i-request-a-millie-or-nat'
+,profPicSource:     'millieicon.png'
 }
 
-var CopyThisAndChangeTheName = {
-name:               ''
-,email:             ''
-,profPicName:       ''
-,profPicAuthor:     ''
-,profPicLink:       ''
-,profPicAuthorLink: ''
-,profPicSource:     ''
+var Borbra = {
+name:               'Borbra Luddington'
+,email:             'borbyowl@findberds.seek'
+,profPicName:       'Borbra with Yowl'
+,profPicAuthor:     'Jane'
+,profPicLink:       'https://mealesque.tumblr.com/post/186778480932/next-in-my-draw-every-s4m-lady-lineup-borbra'
+,profPicAuthorLink: 'https://mealesque.tumblr.com'
+,profPicSource:     'borbraicon.png'
 }
+
+var Dallas = {
+name:               'Dallas Smuth'
+,email:             'thejollypainter@relax.maan'
+,profPicName:       'A happy Dallas'
+,profPicAuthor:     'Lee'
+,profPicLink:       'https://leestei.tumblr.com/post/188773162271/youre-my-friend'
+,profPicAuthorLink: 'https://leestei.tumblr.com'
+,profPicSource:     'dallasicon.png'
+}
+
+var Gerry = {
+name:               'Gerry Podunk'
+,email:             'ilikemoney@broccoli.baby'
+,profPicName:       'Gerry yelling'
+,profPicAuthor:     'Charlie'
+,profPicLink:       'https://flugmunk.tumblr.com/post/187429857750/brongoli'
+,profPicAuthorLink: 'https://flugmunk.tumblr.com'
+,profPicSource:     'gerryicon.png'
+}
+
+var Gillis = {
+name:               'Gillis Soco'
+,email:             'musclesmooch4@kittens.cutie'
+,profPicName:       'Gillis holding a little rat'
+,profPicAuthor:     'Remy'
+,profPicLink:       'https://twitter.com/ratoftouille/status/1162144077791416320?s=20'
+,profPicAuthorLink: 'https://twitter.com/ratoftouille'
+,profPicSource:     'gillisicon.png'
+}
+
+var Habit = {
+name:               'Boris Habit'
+,email:             'hello@thehabitat.join'
+,profPicName:       'A painted Habit'
+,profPicAuthor:     'Spoon'
+,profPicLink:       'https://www.deviantart.com/spoonfayse/art/something-about-submarines-807573888'
+,profPicAuthorLink: 'https://twitter.com/spoonfayse'
+,profPicSource:     'habiticon.png'
+}
+
+var Jerafina = {
+name:               'Jerafina Tabouli'
+,email:             'jlt@h.k12.wy.us'
+,profPicName:       'A lovey-dovey Jerafina'
+,profPicAuthor:     'Napstachill'
+,profPicLink:       'https://napstachill.tumblr.com/post/187417768433/tipsy-lady'
+,profPicAuthorLink: 'https://napstachill.tumblr.com'
+,profPicSource:     'jerafinaicon.png'
+}
+
+var Kamal = {
+name:               'Kamal Bora'
+,email:             'bunnies66@WYSU.mountain'
+,profPicName:       'Kamal smug edition'
+,profPicAuthor:     'Joey'
+,profPicLink:       'https://rork-s.tumblr.com/post/618076875595628544/dont-forget-to'
+,profPicAuthorLink: 'https://rork-s.tumblr.com'
+,profPicSource:     'kamalicon.png'
+}
+
+var Lulia = {
+name:               'Lulia Fame'
+,email:             'scarletstarlet@baloney.hut'
+,profPicName:       'Lulia hanging out with Tiff'
+,profPicAuthor:     'Venus'
+,profPicLink:       'https://fmab.tumblr.com/post/185925858608/i-love-these-red-and-blue-wlw-im-going-fucking'
+,profPicAuthorLink: 'https://fmab.tumblr.com'
+,profPicSource:     'luliaicon.png'
+}
+
+var Marv = {
+name:               'Marv Truncler'
+,email:             'thinkingboutthosbeans@fishfinder.now'
+,profPicName:       'Marv fishing up Tim Tam'
+,profPicAuthor:     'Clover'
+,profPicLink:       'https://cloverlady.tumblr.com/post/186261822575/oh'
+,profPicAuthorLink: 'https://cloverlady.tumblr.com'
+,profPicSource:     'marvicon.png'
+}
+
+var Mirphy = {
+name:               'Mirphy Fotoparat'
+,email:             'mirphyfotos@flashmail.zoom'
+,profPicName:       'Mirphy’s gallery'
+,profPicAuthor:     'Gummyguts'
+,profPicLink:       'https://twitter.com/gummyguts/status/1261762471540461575?s=20'
+,profPicAuthorLink: 'https://twitter.com/gummyguts'
+,profPicSource:     'mirphyicon'
+}
+
+var Nat Vancey = {
+name:               'Nat Vancey'
+,email:             'vanceyvanity@blogblot.cob'
+,profPicName:       'Nat and Trevor being scary'
+,profPicAuthor:     'Gato'
+,profPicLink:       'https://leogatorar.tumblr.com/post/186970129498/and-heres-another-thingy-in-which-i-just-added-a'
+,profPicAuthorLink: 'https://leogatorar.tumblr.com'
+,profPicSource:     'naticon.png'
+}
+
+var Parsley = {
+name:               'Parsley Botch'
+,email:             'parsleybotch@lawyerup.now'
+,profPicName:       'Phineas and Ferb meme but Parsley'
+,profPicAuthor:     'Curious-Shadow-Cat'
+,profPicLink:       '
+https://curious-shadow-cat.tumblr.com/post/188355617862/when-you-find-out-your-dad-adopted-your-friends'
+,profPicAuthorLink: 'https://curious-shadow-cat.tumblr.com'
+,profPicSource:     'parsleyicon.png'
+}
+
+var Putunia = {
+name:               'Putunia Mollar'
+,email:             'punchyhero@cloudmail.cob'
+,profPicName:       'Putunia winking'
+,profPicAuthor:     'Cheri'
+,profPicLink:       'https://twitter.com/bonsaisonly/status/1147343425563504651?s=20'
+,profPicAuthorLink: 'https://twitter.com/bonsaisonly'
+,profPicSource:     'putuniaicon.png'
+}
+
+var Questionette = {
+name:               'Questionette'
+,email:             'fkjsdashjf@jfkdsld.rer'
+,profPicName:       'Questionette posing with Randy'
+,profPicAuthor:     'Alison'
+,profPicLink:       'https://twitter.com/spectrotoons/status/1136810456813907968?s=20'
+,profPicAuthorLink: 'https://twitter.com/spectrotoons'
+,profPicSource:     'questionetteicon.png'
+}
+
+var Randy = {
+name:               'Randy Hapukurk'
+,email:             'pickleboy@meow.now'
+,profPicName:       'A happy Randy'
+,profPicAuthor:     'Hallie'
+,profPicLink:       'https://hccupit.tumblr.com/post/190675087246/finally-played-smile-for-me-the-other-day-randy'
+,profPicAuthorLink: 'https://hccupit.tumblr.com/'
+,profPicSource:     'randyicon.png'
+}
+
+var Ronbo = {
+name:               'Ronbo'
+,email:             'cheeseburgerman@clowning.cob'
+,profPicName:       'A perturbed Ronbo'
+,profPicAuthor:     'Rockformed'
+,profPicLink:       'https://rocky-road-art.tumblr.com/post/187309604148/everybodys-favorite-clown-bonobo-ronald-ringo'
+,profPicAuthorLink: 'https://rocky-road-art.tumblr.com'
+,profPicSource:     'ronboicon.png'
+}
+
+var Tiff = {
+name:               'Tiff Webber'
+,email:             'tiffy@thejuke.box'
+,profPicName:       'A paper cutout Tiff'
+,profPicAuthor:     'Sammi'
+,profPicLink:       'https://goatpaste.tumblr.com/post/186532566150/more-smile-for-me-drawings-i-did-in-my-stream'
+,profPicAuthorLink: 'https://goatpaste.tumblr.com'
+,profPicSource:     'tifficon.png'
+}
+
+var Tim Tam = {
+name:               'Tiff Webber'
+,email:             'iwillgetyou@rocks.fist'
+,profPicName:       'Tim Tam beholding the duck'
+,profPicAuthor:     'David Oneacre'
+,profPicLink:       'https://twitter.com/davidoneacre/status/1141520083426205696?s=20'
+,profPicAuthorLink: 'https://twitter.com/davidoneacre'
+,profPicSource:     'timtamicon.png'
+}
+
+var Trencil = {
+name:               'Trencil Varnia'
+,email:             'gardeninggardenia@mail2.cob'
+,profPicName:       'A variety of Trencils'
+,profPicAuthor:     'Vlad'
+,profPicLink:       'https://bluntforceheadtrauma.tumblr.com/post/185902918737/yes-i-recall-boyhood-as-if-it-were-a-mere-century'
+,profPicAuthorLink: 'https://bluntforceheadtrauma.tumblr.com'
+,profPicSource:     'trencilicon.png'
+}
+
+var Trevor = {
+name:               'Trevor Garbo'
+,email:             'xxfearlesswerewolfxx@pineapples.papples'
+,profPicName:       'Trevor but very very small'
+,profPicAuthor:     'Val'
+,profPicLink:       'https://valthetiredmonster.tumblr.com/post/185850417782/a-small-sketch-turned-color-trevor-really-is-a'
+,profPicAuthorLink: 'https://valthetiredmonster.tumblr.com'
+,profPicSource:     'trevoricon.png'
+}
+
 
 //Here, write an entry in the format:
 //
